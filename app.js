9782d1a1ef1b7f8691e054deafbb242471a34b86
jordancrt <jordan.crt@outlook.com>
2025-10-16 19:50:35 +0200

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{
-  document.querySelectorAll('[data-year]').forEach(el=>el.textContent=new Date().getFullYear());
-  if('serviceWorker' in navigator){ window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js')); }
-});
