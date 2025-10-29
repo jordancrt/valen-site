f2b1469104810210e6f27327ead79901556ae870
jordancrt <jordan.crt@outlook.com>
2025-10-29 22:30:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-
+const CACHE='valenhub-v1';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/assets/style.css','/assets/app.js'])));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
